95c9d85f5cde6e55d595640222ea9a78c59fab99
Hibri Marzook <hibri@hibri.net>
2013-02-24 19:53:40 +0000

    Create basic node

---
+++ src/TreePath/Node.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace TreePath.Unit.Tests
+{
+	public class Node
+	{
+		private readonly string _nodeValue;
+		private List<Node> _children = new List<Node>();
+
+		public Node(string nodeValue) {
+			_nodeValue = nodeValue;
+		}
+		public override string ToString()
+		{
+			return _nodeValue;
+		}
+
+		public void AddChild(Node node) {
+			_children.Add(node);
+		}
+
+		public IList<Node> GetChildren() {
+			return _children;
+		}
+	}
+}
--- src/TreePath/TreePathTests.cs
+++ src/TreePath/TreePathTests.cs
@@ -6,8 +6,26 @@ namespace TreePath.Unit.Tests
 	public class TreePathTests
     {
 		[Test]
-		public void Should_foo() {
-			Assert.That(1, Is.EqualTo(1));
+		public void Should_add_root() {
+			var node = BuildTree("/home/");
+
+			Assert.That(node.ToString(), Is.EqualTo("home"));
+		}
+
+		[Test]
+		public void Should_add_first_child() {
+			var first_leaf = BuildTree("/home/sports/");
+
+			Assert.That(first_leaf.GetChildren()[0].ToString(), Is.EqualTo("sports"));
+		}
+
+		private Node BuildTree(string path) {
+			string[] pathElements = path.Split('/');
+			string rootNodeValue = pathElements[1];
+
+			var rootNode = new Node(rootNodeValue);
+			rootNode.AddChild(new Node(pathElements[2]));
+			return rootNode;
 		}
     }
 }
