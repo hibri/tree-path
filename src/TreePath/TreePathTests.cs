d26f02b2fda2921d94f5c5430f779eb27af6811f
Hibri Marzook <hibri@hibri.net>
2013-02-24 20:39:34 +0000

    add tests for part 1

--- src/TreePath/TreePathTests.cs
+++ src/TreePath/TreePathTests.cs
@@ -27,9 +27,39 @@ namespace TreePath.Unit.Tests
 			var rootNode = new Node("/");
 			rootNode.AddPath("/home/sports/");
 			rootNode.AddPath("/home/music/");
+
 			var anotherChild = rootNode.GetChildren()[0].GetChildren()[1];
 
 			Assert.That(anotherChild.ToString(), Is.EqualTo("music"));
+
+		}
+
+		[Test]
+		public void Should_add_many_children_and_grand_children() {
+			var rootNode = new Node("/");
+			rootNode.AddPath("/home/sports/basketball");
+			rootNode.AddPath("/home/sports/football");
+			rootNode.AddPath("/home/sports/basketball/NCAA");
+
+			rootNode.AddPath("/home/music/");
+			rootNode.AddPath("/home/music/rap");
+			rootNode.AddPath("/home/music/rock");
+			rootNode.AddPath("/home/music/rap/gangster");
+
+			Assert.That(rootNode.GetChildren()[0].GetChildren()[0].ToString(), Is.EqualTo("sports"));
+			Assert.That(rootNode.GetChildren()[0].GetChildren()[0].GetChildren()[0].ToString(), Is.EqualTo("basketball"));
+			Assert.That(rootNode.GetChildren()[0].GetChildren()[0].GetChildren()[1].ToString(), Is.EqualTo("football"));
+			Assert.That(rootNode.GetChildren()[0].GetChildren()[0].GetChildren()[0].GetChildren()[0].ToString(), Is.EqualTo("NCAA"));
+
+			Assert.That(rootNode.GetChildren()[0].GetChildren()[1].ToString(), Is.EqualTo("music"));
+			Assert.That(rootNode.GetChildren()[0].GetChildren()[1].GetChildren()[0].ToString(), Is.EqualTo("rap"));
+			Assert.That(rootNode.GetChildren()[0].GetChildren()[1].GetChildren()[1].ToString(), Is.EqualTo("rock"));
+			Assert.That(rootNode.GetChildren()[0].GetChildren()[1].GetChildren()[0].GetChildren()[0].ToString(), Is.EqualTo("gangster"));
+
+
+
+
+
 
 		}
     }
