6766bd85d8eaf35eb43c47cfe6e0722431ad3598
Hibri Marzook <hibri@hibri.net>
2013-02-24 19:32:42 +0000

    initial commit

---
+++ src/TreePath/TreePathTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace TreePath.Unit.Tests
+{
+	[TestFixture]
+	public class TreePathTests
+    {
+		[Test]
+		public void Should_foo() {
+			Assert.That(1, Is.EqualTo(1));
+		}
+    }
+}
